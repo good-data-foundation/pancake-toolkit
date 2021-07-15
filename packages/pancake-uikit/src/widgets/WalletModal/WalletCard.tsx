cbabba06a4a5176e1a21bcfab374e08b336c423a
hachiojidev <hachiojidev@protonmail.com>
2021-07-15 12:53:45 +0900

    feat(uikit): Save last connected wallet

--- packages/pancake-uikit/src/widgets/WalletModal/WalletCard.tsx
+++ packages/pancake-uikit/src/widgets/WalletModal/WalletCard.tsx
@@ -4,7 +4,7 @@ import Button from "../../components/Button/Button";
 import Text from "../../components/Text/Text";
 import MoreHoriz from "../../components/Svg/Icons/MoreHoriz";
 import { ButtonProps } from "../../components/Button";
-import { connectorLocalStorageKey } from "./config";
+import { connectorLocalStorageKey, walletLocalStorageKey } from "./config";
 import { Login, Config } from "./types";
 
 interface Props {
@@ -27,21 +27,23 @@ export const MoreWalletCard: React.FC<ButtonProps> = (props) => {
   return (
     <WalletButton variant="tertiary" {...props}>
       <MoreHoriz width="40px" mb="8px" color="textSubtle" />
-      <Text bold color="textSubtle" fontSize="14px">
-        More
-      </Text>
+      <Text fontSize="14px">More</Text>
     </WalletButton>
   );
 };
 
 const WalletCard: React.FC<Props> = ({ login, walletConfig, onDismiss }) => {
   const { title, icon: Icon } = walletConfig;
+
   return (
     <WalletButton
       variant="tertiary"
       onClick={() => {
         login(walletConfig.connectorId);
-        window.localStorage.setItem(connectorLocalStorageKey, walletConfig.connectorId);
+
+        localStorage.setItem(connectorLocalStorageKey, walletConfig.connectorId);
+        localStorage.setItem(walletLocalStorageKey, walletConfig.title);
+
         onDismiss();
       }}
       id={`wallet-connect-${title.toLocaleLowerCase()}`}
